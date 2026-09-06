3fa31008956fa45361b4fc427c06dba4340f73cf
waxeee57-cyber <waxeee57@gmail.com>
2026-09-06 03:06:19 +0200

    Add JS part loaders for data-URI course

---
+++ app_loader.js
@@ -0,0 +1,6 @@
+
+(function(){
+  var s = (window.__S7_APP_PARTS||[]).join('');
+  window.__S7_APP_PARTS = null;
+  (0,eval)(s);
+})();
---
+++ content_loader.js
@@ -0,0 +1,6 @@
+
+(function(){
+  var s = (window.__S7_CONTENT_PARTS||[]).join('');
+  window.__S7_CONTENT_PARTS = null;
+  (0,eval)(s);
+})();
